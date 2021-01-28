2c91f41790aaaa1706929d799b8998c00db49487
Morgan <55920765+momo007Dev@users.noreply.github.com>
2021-01-28 12:00:37 +0100

    UI VLSM et subnet finie, reste a implementer la loqique de calcul vlsm

--- code/utils.py
+++ code/utils.py
@@ -43,16 +43,43 @@ class blueprintFunctions:
         btn.setFont(font_btn)
         btn.setText("Home")
 
+    def mkGroupBox (gb, geometry, text):
+        gb.setGeometry(geometry)
+        gb.setFont(font_gb)
+        gb.setTitle(text)
+        gb.setObjectName(str(gb))
+
     def mkCombo (combo, geometry, style):
         combo.setGeometry(geometry)
         combo.setFont(font_linedit)
         combo.setStyleSheet(style)
         combo.setObjectName(str(combo))
 
+    # ---------------------------
     # Fills combo with /32 => /0
+    #----------------------------
     def fillComboCidr (combo):
         for i in reversed(range(0,33)):
             combo.addItem("/" + str(i))
+
+    def mkTable (table, geometry, style, col, row):
+        table.setGeometry(geometry)
+        table.setFont(font_table)
+        table.setStyleSheet(style)
+        table.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        table.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        table.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        table.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
+        table.setObjectName(str(table))
+        table.setColumnCount(col)
+        table.setRowCount(row)
+        table.verticalHeader().setVisible(False)
+
+    def addDataTable (table, position, text):
+        item = QtWidgets.QTableWidgetItem()
+        item.setText(text)
+        table.setHorizontalHeaderItem(position, item)
 
 #----------------
 #   FONT USED
@@ -73,6 +100,15 @@ font_linedit.setFamily("Verdana")
 font_linedit.setPointSize(10)
 font_linedit.setBold(True)
 font_linedit.setWeight(75)
+
+font_gb = QtGui.QFont()
+font_gb.setPointSize(12)
+font_gb.setBold(True)
+font_btn.setUnderline(False)
+font_gb.setWeight(75)
+
+font_table = QtGui.QFont()
+font_table.setPointSize(9)
 
 #---------------------
 #   STYLESHEETS USED
